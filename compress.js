f2a9f2eb30e25b9121f27e0dbceb31d78b22ee63
TheXReflex <xaviercoppens5@gmail.com>
2016-01-01 17:17:14 +0100

    compress.js

---
+++ compress.js
@@ -0,0 +1,70 @@
+function bitimize(byte){
+  var curr, prev, start = true, bit_count = 1, output = "";
+  byte = byte + " ";
+  for(var i = 0; i < byte.length; i ++){
+	curr = byte.charAt(i);
+	
+	if(!start){
+	  
+	  if(curr == prev){
+		bit_count ++;
+	  } else {
+		if(bit_count >= 2){
+		  output += bit_count + prev;
+		} else {
+		  output += prev;
+		}
+		bit_count = 1;
+	  }
+	  
+	} else {
+	  start = false;
+	}
+	
+	prev = curr;
+  }
+  
+  return output;
+}
+
+function compress(uncompressed){
+  //define variables
+  var curr, prev, byte_start, bytes, byte_count, input, output;
+  byte_start = true;
+  byte_count = 1;
+  input = uncompressed + " ";
+  output = "";
+  
+  bytes = input.split(" "); 
+  
+  //loop through bytes
+  for(var i = 0; i < bytes.length; i ++){
+	
+	curr = bytes[i];
+	
+	if(!byte_start){
+	  
+	  if(curr == prev){
+		byte_count ++;
+	  } else {
+		
+		if(byte_count >= 2){
+		  output += byte_count + "{" + bitimize(prev) + "}";
+		} else {
+		  output += "{"+bitimize(prev)+"}";
+		}
+		
+		
+		byte_count = 1;
+	  }
+	  
+	} else {
+	  byte_start = false;
+	}
+	
+	//update previous
+	prev = curr;
+  }
+  
+  return output;
+}
